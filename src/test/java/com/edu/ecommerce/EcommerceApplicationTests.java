59df5eac6885b37bc3961e579f93193354836487
kajuga <msg2fedorov@gmail.com>
2022-06-22 18:41:19 +0300

    Initial commit

---
+++ src/test/java/com/edu/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

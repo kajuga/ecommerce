15afbd7ef99844d7b25a55ced4b3910e89fec6d5
kajuga <msg2fedorov@gmail.com>
2022-06-27 11:32:00 +0300

    product api added

---
+++ src/main/java/com/edu/ecommerce/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.edu.ecommerce.service;
+
+import com.edu.ecommerce.dto.product.ProductDto;
+import com.edu.ecommerce.model.Category;
+import com.edu.ecommerce.model.Product;
+import com.edu.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void addProduct(ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        productRepository.save(product);
+    }
+
+    public static Product getProductFromDto(ProductDto productDto, Category category) {
+        Product product = new Product();
+        product.setCategory(category);
+        product.setDescription(productDto.getDescription());
+        product.setImageURL(productDto.getImageURL());
+        product.setPrice(productDto.getPrice());
+        product.setName(productDto.getName());
+        return product;
+    }
+}

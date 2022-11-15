0d6161a2c150ae16d0604143970c73a56dd75d18
kajuga <msg2fedorov@gmail.com>
2022-11-15 14:34:48 +0300

    Добавил в тест UserController получение всех user'a by ID, create user. //TODO update user

--- src/test/java/com/edu/ecommerce/controllers/UserControllerTest.java
+++ src/test/java/com/edu/ecommerce/controllers/UserControllerTest.java
@@ -13,22 +13,36 @@ import com.edu.ecommerce.service.impl.RoleServiceImpl;
 import com.edu.ecommerce.service.interfaces.RoleService;
 import com.edu.ecommerce.service.interfaces.UserService;
 import com.edu.ecommerce.util.TestUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,6 +50,7 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -73,10 +88,17 @@ class UserControllerTest {
     private static final String BASE_REQUEST = "json/request/";
     private static final String BASE_RESPONSE = "json/response/";
 
+    private static final String REQUEST_CREATE_1 = BASE_REQUEST + "user_create_1.json";
+    private static final String REQUEST_UPDATE_1 = BASE_REQUEST + "user_update_new_1.json";
+
+    private static final String RESPONSE_CREATE_1 = BASE_RESPONSE + "user_create_1.json";
+    private static final String RESPONSE_UPDATE_OLD_1 = BASE_RESPONSE + "user_update_old_1.json";
+//    private static final String RESPONSE_UPDATE_1 = BASE_RESPONSE + "user_update_old_1.json";
+    private static final String RESPONSE_GET_1 = BASE_RESPONSE + "user_get_1.json";
     private static final String RESPONSE_GET_3 = BASE_RESPONSE + "user_get_3.json";
 
 
-        @Test
+    @Test
     void getUsersTest() throws Exception {
         final List<UserDto> response = TestUtil.readJsonResourceToList(RESPONSE_GET_3, UserDto.class);
         final String expected = TestUtil.write(response);
@@ -99,5 +121,91 @@ class UserControllerTest {
                 .andExpect(content().json(expected));
     }
 
+
+    @Test
+    void getUserByIdSuccessTest() throws Exception {
+
+        final UserDto response = TestUtil.readJsonResource(RESPONSE_GET_1, UserDto.class);
+        final String expected = TestUtil.write(response);
+
+            when(roleService.findById(anyLong())).thenAnswer(invocationOnMock -> {
+                var role = new UserRole();
+                role.setId(invocationOnMock.getArgument(0, Long.class));
+                return role;
+            });
+
+        doReturn(mapper.fromDto(response))
+                .when(userService)
+                .findById(2L);
+
+        this.mockMvc
+                .perform(get("/users/{id}", 2L))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expected));
+    }
+
+
+    @Test
+    void createUserSuccessTest() throws Exception {
+            final UserDto request = TestUtil.readJsonResource(REQUEST_CREATE_1, UserDto.class);
+            final UserDto response = TestUtil.readJsonResource(RESPONSE_CREATE_1, UserDto.class);
+            final String expected = TestUtil.write(response);
+
+        when(roleService.findById(anyLong())).thenAnswer(invocationOnMock -> {
+            var role = new UserRole();
+            role.setId(invocationOnMock.getArgument(0, Long.class));
+            return role;
+        });
+
+            doReturn(mapper.fromDto(response))
+                    .when(userService)
+                    .create(any(User.class));
+
+            this.mockMvc
+                    .perform(MockMvcRequestBuilders.post("/users")
+                            .with(csrf())
+                            .content(asJsonString(request))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andDo(print())
+                    .andExpect(content().string(expected))
+                    .andExpect(status().isCreated());
+        }
+
+//        @Test
+//        void updateUserSuccessTest() throws Exception {
+//            final UserDto request = TestUtil.readJsonResource(REQUEST_UPDATE_1, UserDto.class);
+//            final UserDto response = TestUtil.readJsonResource(RESPONSE_UPDATE_OLD_1, UserDto.class);
+//            final String expected = TestUtil.write(response);
+//
+//            when(roleService.findById(anyLong())).thenAnswer(invocationOnMock -> {
+//                var role = new UserRole();
+//                role.setId(invocationOnMock.getArgument(0, Long.class));
+//                return role;
+//            });
+//
+//            this.mockMvc
+//                    .perform(MockMvcRequestBuilders.put("/users/{id}", 2L)
+//                            .with(csrf())
+//                            .content(asJsonString(request))
+//                            .contentType(MediaType.APPLICATION_JSON))
+//                    .andDo(MockMvcResultHandlers.print())
+//                    .andDo(print())
+//                    .andExpect(content().string(expected))
+//                    .andExpect(status().isOk());
+//
+//        }
+
+
+
+    static String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
